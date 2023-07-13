1cb5f34b34349c7fe1b1849c73fe98c83bf46682
ElsaLecureux <elsa.lecureux@gmail.com>
2023-07-13 16:22:16 +0200

    pair programming, starting adding sign in with user controller

---
+++ Controlers/userController.js
@@ -0,0 +1,18 @@
+const datamapper = require("../dataMapper");
+
+const userController = {
+
+  // allcategories method
+  login: async function (request, response) {
+    try {
+      console.log(request.body);
+     } catch (error) {
+      console.log(error);
+      response.status(500).render('error', {
+        message: 'Problème serveur, réessayez plus tard.',
+      });
+    };
+  }
+};
+
+module.exports = userController;
--- router.js
+++ router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mainController = require('./Controlers/mainController');
 const categoriesController = require('./Controlers/categoriesController');
+const userController = require('./Controlers/userController');
 const router= express.Router();
 
 router.get('/recipes', mainController.homePage);
 router.get('/recipe/:id', mainController.recipeDetails);
 router.get('/categories', categoriesController.allCategories);
+router.post('/user', userController.login);
 
 module.exports = router;
